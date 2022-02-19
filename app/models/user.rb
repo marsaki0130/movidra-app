a4224181a26bb27c7911c238e8539701132964eb
正木啓介 <kettan0130@gmail.com>
2022-02-19 10:03:54 +0900

    deviseのviewを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
 end
