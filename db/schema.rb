d0a3b015422754f9c9df7146aaac901837b05da6
正木啓介 <kettan0130@gmail.com>
2022-02-23 11:00:11 +0900

    commentモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_081709) do
+ActiveRecord::Schema.define(version: 2022_02_23_015227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_02_19_081709) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.text "feelings"
+    t.text "consideration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
   end
 
   create_table "movies", force: :cascade do |t|
