56426af35f0ada765d0a0c5deed846d1750ed606
正木啓介 <kettan0130@gmail.com>
2022-02-26 10:05:08 +0900

    flashを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,17 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.build(movie_params)
+    if @comment.save
+      redirect_to movie_path(@movie), notice:'コメントを追加'
+    else
+      flash.now[:error] = '保存に失敗しました' 
+      render :new
+    end
+  end
+
+  def movie_params
+    params.require(:comment).permit(:feelings) 
   end
 end
