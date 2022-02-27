aa14a2fc942b3dcbac1dacc33b1e97d5fe33e20b
正木啓介 <kettan0130@gmail.com>
2022-02-28 08:13:55 +0900

    ui微調整

--- app/controllers/spoilers_controller.rb
+++ app/controllers/spoilers_controller.rb
@@ -1,8 +1,12 @@
 class SpoilersController < ApplicationController
   def index
+    @movie = Movie.find(params[:movie_id])
+    @spoiler = @movie.spoilers
   end
 
   def new
+    movie = Movie.find(params[:movie_id])
+    @spoiler = movie.spoilers.build
   end
 
   def create
