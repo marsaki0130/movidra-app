6a7d6699ed15d41555ac704c102ccc9b436cb971
正木啓介 <kettan0130@gmail.com>
2022-03-01 07:22:55 +0900

    コントローラー調整

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @movie = Movie.all
   end
--- app/controllers/spoilers_controller.rb
+++ app/controllers/spoilers_controller.rb
@@ -10,5 +10,17 @@ class SpoilersController < ApplicationController
   end
 
   def create
+    @movie = Movie.find(params[:movie_id]) 
+    @spoiler = @movie.spoilers.build(spoiler_params)
+    if @spoiler.save
+      redirect_to movie_path(@movie), notice: 'コメントを追加しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  def spoiler_params
+    params.require(:spoiler).permit(:content) 
   end
 end
