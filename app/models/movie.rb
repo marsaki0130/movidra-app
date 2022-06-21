7c92a6dab84d1c10fd12b1e79b16cd27a8f27afc
正木啓介 <kettan0130@gmail.com>
2022-06-22 08:27:21 +0900

    movie-indexで星を表示させる

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
 
   has_many_attached :eyecatch
-  has_many :comments,dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :spoilers, dependent: :destroy
   has_many :likes, dependent: :destroy
 
